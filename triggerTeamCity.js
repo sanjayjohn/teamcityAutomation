b28e5e3cc31f42962421f4450392eea05d1f6150
Sanjay John <sjphilip18@gmail.com>
2018-07-13 16:26:49 -0400

    Pulse Rule for Triggering Team City

---
+++ triggerTeamCity.js
@@ -0,0 +1,33 @@
+var url = "http://" + constants.BambooURL + '/app/rest/buildQueue';
+
+var headers = {
+   'Content-Type': 'application/xml'
+};
+
+let projectCode = constants.TeamCityProjectCode
+
+var dataString = "<build><buildType id='" + projectCode + "'/></build>";
+
+var opts = {
+    url: url,
+    method: 'POST',
+    headers: headers,
+    body: dataString,
+    auth: {
+       "user": constants.TeamCityUserName,
+       "pass": constants.TeamCityPassword
+    }
+};
+
+request.post(opts, function(err, res, bd) {
+        if(err) {
+            Promise.reject(err);
+            emitEvent('SlackEvent', { TeamCityProject: "Unexpected Error Triggering TeamCity Build" }); 
+
+
+        }
+        else {
+            emitEvent('SlackEvent', { TeamCityTriggerSuccess: "TeamCity Build just kicked off for this plan " + projectCode }); 
+
+        }
+})
